c05d70685dc4cc63f7cb69e502b1e49e7d705718
章啸 <longfengno2@gmail.com>
2018-11-21 21:21:50 +0800

    change port

--- server.js
+++ server.js
@@ -8,14 +8,13 @@ const router = require('./router');
 const resolve = file => path.resolve(__dirname, file);
 
 const app = new Koa();
-console.log(process.env.port);
 
 // 将请求体转换为 JSON 的中间件
 app.use(bodyParser());
 // 路由处理
 router(app)
 
-const server = app.listen(5387, () => {
+const server = app.listen(9000, () => {
     console.log('application start');
 });
 
