4b22eb657a13b28ff2bf760bf2659d2abde40383
章啸 <longfengno2@gmail.com>
2018-11-21 16:01:41 +0800

    init:tada:

---
+++ server.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const Koa = require('koa');
+const bodyParser = require('koa-bodyparser');
+
+const ws = require('./ws');
+const router = require('./router');
+
+const resolve = file => path.resolve(__dirname, file);
+
+const app = new Koa();
+console.log(process.env.port);
+
+// 将请求体转换为 JSON 的中间件
+app.use(bodyParser());
+// 路由处理
+router(app)
+
+const server = app.listen(9000, () => {
+    console.log('application start');
+});
+
+// 开启webscoket
+ws(server);
